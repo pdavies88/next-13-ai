02f8274d8c996733873ad6d0a451c7934d125476
Preston Davies <preston.davies@get10up.com>
2023-05-16 19:51:25 -0400

    Single User CRUD

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [myPrompts, setMyPrompts] = useState([]);
 
@@ -14,17 +15,40 @@ const MyProfile = () => {
     const fetchPrompts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/prompts`);
       const data = await response.json();
-      console.log(response);
+
       setMyPrompts(data);
     };
 
-    console.log(myPrompts);
-    if (session?.user.id) fetchPrompts();
+    if (session?.user.id) {
+      fetchPrompts();
+    }
   }, [session?.user.id]);
 
-  const handleEdit = () => {};
+  const handleEdit = (prompt) => {
+    router.push(`/update-prompt?id=${prompt._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (prompt) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPrompts = myPrompts.filter(
+          (item) => item._id !== prompt._id
+        );
+
+        setMyPrompts(filteredPrompts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <>
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -57,7 +57,7 @@ const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(prompt.tag)}
       >
-        {prompt.tag}
+        #{prompt.tag}
       </p>
       {session?.user.id === prompt.creator._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
